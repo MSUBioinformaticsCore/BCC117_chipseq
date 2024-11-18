fe516d200acf8a2029897fb43f121d1156ca5d56
Stephanie Hickey <hickeys6@msu.edu>
2024-11-18 11:57:22 -0500

    update

--- src/getConsensusPeaks.R
+++ src/getConsensusPeaks.R
@@ -8,7 +8,7 @@ library(consensusSeekeR)
 
 chrom_file = args[1]
 # chrom_file = "/mnt/research/bioinformaticsCore/shared/Genomes/arabidopsis_thaliana/ensembl_release60/sizes.genome"
-chromsizes = read.delim(args[1])
+chromsizes = read.delim(args[1], header=F)
 # chromsizes = read.delim("/mnt/research/bioinformaticsCore/shared/Genomes/arabidopsis_thaliana/ensembl_release60/sizes.genome", header=F)
 
 chrInfo <- Seqinfo(seqnames=chromsizes$V1,
@@ -18,7 +18,7 @@ chrInfo <- Seqinfo(seqnames=chromsizes$V1,
 narrow_peak_path = args[2]
 # narrow_peak_path = "/mnt/research/bioinformaticsCore/projects/thomashowm/BCC117_chipseq/results/bwa/merged_library/macs3/narrow_peak"
 narrow_peak_files = list.files(narrow_peak_path, 
-                               pattern = ".narrowPeak",
+                               pattern = ".narrowPeak$",
                                full.names = TRUE)
 pattern = args[3]
 # pattern = "cold"
@@ -51,7 +51,7 @@ results <- findConsensusPeakRegions(
   peaks = peak_unlist,
   extendingSize = round(median_width/2),
   chrInfo = chrInfo,
-  expandToFitPeakRegion = FALSE,
+  expandToFitPeakRegion = TRUE,
   shrinkToFitPeakRegion = TRUE,
   minNbrExp = length(narrow_peak_files)-1,
   nbrThreads = 16)

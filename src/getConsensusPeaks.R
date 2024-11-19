4342f8bf2c6dbce121a783905505786c3f846bd1
Stephanie Hickey <hickeys6@msu.edu>
2024-11-19 16:48:18 -0500

    update

--- src/getConsensusPeaks.R
+++ src/getConsensusPeaks.R
@@ -2,62 +2,102 @@
 #' @param args[2] path to narrowPeak files 
 #' @param args[3] pattern
 
-args <- commandArgs(TRUE)
-library(consensusSeekeR)
-# Chromosomes information
+args <- commandArgs(TRUE) # Capture command line arguments
+library(consensusSeekeR) # Load the consensusSeekeR library
 
-chrom_file = args[1]
+# Chromosomes information
+chrom_file = args[1] # Path to chromosome sizes file
 # chrom_file = "/mnt/research/bioinformaticsCore/shared/Genomes/arabidopsis_thaliana/ensembl_release60/sizes.genome"
-chromsizes = read.delim(args[1], header=F)
+chromsizes = read.delim(args[1], header=F) # Read chromosome sizes file
 # chromsizes = read.delim("/mnt/research/bioinformaticsCore/shared/Genomes/arabidopsis_thaliana/ensembl_release60/sizes.genome", header=F)
 
+# Create chromosome information object
 chrInfo <- Seqinfo(seqnames=chromsizes$V1,
-                   seqlengths=chromsizes$V2, isCircular=rep(FALSE, nrow(chromsizes)),
+                   seqlengths=chromsizes$V2, 
+                   isCircular=rep(FALSE, nrow(chromsizes)),
                    genome=chrom_file)
 
-narrow_peak_path = args[2]
+narrow_peak_path = args[2] # Path to narrowPeak files
 # narrow_peak_path = "/mnt/research/bioinformaticsCore/projects/thomashowm/BCC117_chipseq/results/bwa/merged_library/macs3/narrow_peak"
+
+# List all narrowPeak files in the directory
 narrow_peak_files = list.files(narrow_peak_path, 
                                pattern = ".narrowPeak$",
                                full.names = TRUE)
-pattern = args[3]
-# pattern = "cold"
-narrow_peak_files = narrow_peak_files[grepl(pattern, narrow_peak_files)]
 
-narrowPeak_list = list()
-peak_list = list()
-  
+pattern = args[3] # Pattern to filter narrowPeak files
+# pattern = "cold"
+narrow_peak_files = narrow_peak_files[grepl(pattern, narrow_peak_files)] # Filter files based on pattern
+
+narrowPeak_list = list() # Initialize list for narrowPeak data
+peak_list = list() # Initialize list for peak data
+
+# Process each narrowPeak file
 for(file in narrow_peak_files){
   
-  REP = basename(file)
-  result = readNarrowPeakFile(file, extractRegions = TRUE, extractPeaks = TRUE)
-  narrowPeak_list[[REP]] = result$narrowPeak
-  peak_list[[REP]] = result$peak
+  REP = basename(file) # Get base name of the file
+  result = readNarrowPeakFile(file, extractRegions = TRUE, extractPeaks = TRUE) # Read narrowPeak file
+  narrowPeak_list[[REP]] = result$narrowPeak # Store narrowPeak regions
+  peak_list[[REP]] = result$peak # Store peaks
   
+  # Assign file names to narrowPeak and peak lists
   names(narrowPeak_list[[REP]]) = rep(REP, length(narrowPeak_list[[REP]]))
   names(peak_list[[REP]]) = rep(REP, length(peak_list[[REP]]))
-  
 }
 
+# Create GRangesList objects
 narrowPeak_grl = GRangesList(narrowPeak_list)
-narrowPeak_unlist = unlist(narrowPeak_grl)
+narrowPeak_unlist = unlist(narrowPeak_grl) # Unlist narrowPeak regions
 peak_grl = GRangesList(peak_list)
-peak_unlist =  unlist(peak_grl)
+peak_unlist =  unlist(peak_grl) # Unlist peaks
 
-median_width = median(width(narrowPeak_unlist))
+median_width = median(width(narrowPeak_unlist)) # Calculate median width of narrowPeak regions
 
+# Find consensus peak regions
 results <- findConsensusPeakRegions(
   narrowPeaks = narrowPeak_unlist,
   peaks = peak_unlist,
-  extendingSize = round(median_width/2),
+  extendingSize = round(median_width/2), # Extend size based on median width
   chrInfo = chrInfo,
-  expandToFitPeakRegion = TRUE,
-  shrinkToFitPeakRegion = TRUE,
-  minNbrExp = length(narrow_peak_files)-1,
-  nbrThreads = 16)
+  expandToFitPeakRegion = TRUE, # Expand regions to fit peaks
+  shrinkToFitPeakRegion = TRUE, # Shrink regions to fit peaks
+  minNbrExp = length(narrow_peak_files)-1, # Minimum number of experiments
+  nbrThreads = 16) # Number of threads to use
 
-library(GenomicRanges)
-library(rtracklayer)
-export.bed(format = "bed", 
-           object=results$consensusRanges, 
-           con = paste0(narrow_peak_path, "/", pattern, "_consensus.peaks.bed"))
+# Assign consensus IDs to results
+consensusIDS = as.data.frame(paste0(pattern, "_consensusPeak_", 1:length(results$consensusRanges)))
+colnames(consensusIDS) = "consensusID"
+mcols(results$consensusRanges) <- consensusIDS
+
+library(GenomicRanges) # Load GenomicRanges library
+
+# Find overlaps between narrowPeak regions and consensus ranges
+hits = as.data.frame(findOverlaps(narrowPeak_unlist, results$consensusRanges))
+consensus_hits = as.data.frame(results$consensusRanges[hits$subjectHits]) # Extract consensus ranges
+narrowPeak_hits = as.data.frame(narrowPeak_unlist[hits$queryHits], row.names = NULL) # Extract narrowPeak regions
+
+# Rename columns for clarity
+colnames(consensus_hits)[1:5] = paste0("consensus_", colnames(consensus_hits)[1:5])
+peak2consensus = cbind(narrowPeak_hits, consensus_hits) # Combine narrowPeak and consensus data
+
+# Write detailed peak-to-consensus mapping to a CSV file
+write.csv(peak2consensus, file = paste0(narrow_peak_path, "/", pattern, "_consensus.peaks_info.csv"))
+
+library(tidyverse) # Load tidyverse for data manipulation
+
+# Make the consensus score qValue of the constituent peak with the highest qValue
+consensus_score = 
+  peak2consensus %>%
+  group_by(consensusID) %>%
+  slice_max(qValue, n = 1) %>% # Select peak with highest qValue
+  select(consensus_seqnames, consensus_start, 
+         consensus_end, consensusID, qValue, strand) %>%
+  mutate(strand = ".") # Set strand information
+
+# Write consensus peaks to a BED file
+write.table(consensus_score, 
+            file = paste0(narrow_peak_path, "/", pattern, "_consensus.peaks.bed"),
+            quote = FALSE,
+            sep = "\t",
+            row.names = FALSE,
+            col.names = FALSE)
